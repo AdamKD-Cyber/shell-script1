eaeae92a271da7b4b723a9b1f575b58fd4a06480
AdamKD-Cyber <adamkd.cyber@gmail.com>
2025-05-16 19:46:46 +0300

    loginInfo.var

--- var.sh
+++ var.sh
@@ -1,23 +1,30 @@
 echo -e "\n------------ Authentication Required -------------\n"
-read -p "Insert Username: " username
-read -sp "Insert Password: " password
+
+read -rp "Insert Username: " username
+read -rsp "Insert Password: " password
+
 echo -e "\n\n------------ Your Login Information --------------\n"
+
 hidden=""
 for (( i=0; i<${#password}; i++ )); do
-hiddenP+="*"
+    hidden+="*"
 done
+
 echo "Username: $username"
-echo "Password: $hiddenP"
-read -p "Would you like to save a record? (Y/N): " ans
+echo "Password: $hidden"
+
 while true; do
-if [[ "$ans" == "Y" || "$ans" == "y" ]]; then
-touch loginInfo.txt && echo -e "$username\n$password\n" > loginInfo.txt 
-echo -e "\n-------------- Login Info Saved Successfully --------------\n"
-break
-elif [[ "$ans" == "N" || "$ans" == "n" ]]; then
-echo -e "\n\nYour login info were removed from the system.\n"
-break
-else
-read -p "Misunderstood - Respond only with (Y/N): " ans
-fi
+    read -rp "Would you like to save a record? (Y/N): " ans
+    ans=${ans^^}
+
+    if [[ "$ans" == "Y" ]]; then
+        echo -e "$username\n$password\n" > loginInfo.txt
+        echo -e "\n-------------- Login Info Saved Successfully --------------\n"
+        break
+    elif [[ "$ans" == "N" ]]; then
+        echo -e "\n\nYour login info were removed from the system.\n"
+        break
+    else
+        echo "Please respond only with Y or N."
+    fi
 done
